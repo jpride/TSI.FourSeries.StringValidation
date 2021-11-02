5ebfba103a8ca795993607a99dd8f67def0e82b5
Jeff Pride <jpride@gmail.com>
2021-11-02 18:56:40 -0500

    Add project files.

---
+++ TSI.FourSeries.StringValidation/IPAddressValidator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Crestron.SimplSharp;
+
+
+
+
+namespace TSI.FourSeries.StringValidation
+{
+    public class IPAddressValidator
+
+    {
+        public IPAddressValidator()
+        {
+        }
+
+        /// <summary>
+        /// Returns 0 when ipString is not a valid IP address, 1 when it is
+        /// </summary>
+        /// <param name="ipa">enter the IP address to be validated</param>
+
+  
+
+        public ushort ValidateIPv4Address(string ipString)
+        {
+            if (String.IsNullOrWhiteSpace(ipString))
+            {
+                return 0;
+            }
+
+            string[] splitValues = ipString.Split('.');
+            if (splitValues.Length != 4)
+            {
+                return 0;
+            }
+
+
+            //Ternary Oparator  [condition or Bool ? value if true : value if false] **suing the "_" discarad operator in the output side of TryParse
+            return (ushort) (System.Net.IPAddress.TryParse(ipString, out _) ? 1 : 0);
+        }
+
+
+        public string GetValidIPv4Address(string ipString)
+        {
+            if (ValidateIPv4Address(ipString) == 1)
+            {
+                return ipString;
+            }
+            else
+            {
+                CrestronConsole.PrintLine("Invalid IP Address: {0}", ipString);
+                return string.Empty;
+            }
+        
+        }
+    }
+}
---
+++ TSI.FourSeries.StringValidation/NVXDeviceNameValidator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
+using System.Text.RegularExpressions;
+
+namespace TSI.FourSeries.StringValidation
+{
+    public class NVXDeviceNameValidator
+    {
+
+        public NVXDeviceNameValidator()
+        {
+        }
+
+        /// <summary>
+        /// This method will replace all occurrences of illegal characters defined in the illegalPattern string with the substituteString. It will also remove more than 1 sequential occurrence of the substituteString
+        /// </summary>
+        /// <param name="testString">This is the string to validate</param>
+        /// <param name="substituteString">This is the replacement string for illegal characters</param>
+        /// <returns>Returns a valid string</returns>
+        public string ValidateDeviceName(string testString, string substituteString)
+        {
+            string illegalPattern = @"[\\~`!@#$%^&*()_=+{}|,./\?;:\s]";  //list of illegal characters and space '\s'
+            string repeatCharsPattern = "[" + substituteString + "]{2,}"; //patterns to determine sequential characters of the substitute string, these will be filtered out        
+
+            try
+            {
+                //Remove leading and trailing white space
+                testString = testString.Trim();
+
+                //remove illegal characters from the pattern above
+                string validString = Regex.Replace(testString, illegalPattern, substituteString);
+
+                //remove any repeated substitution chars
+                validString = Regex.Replace(validString, repeatCharsPattern, substituteString);
+
+                //remove replacement string from end of string
+                //validString = entire validString up to last char + a 1 char string that is either not replaced because it is valid, or the empty string because it is the substitution string (Director doesnt like even the allowed '-' at the end of a name
+                validString = validString.Substring(0, validString.Length - 1) + Regex.Replace(validString.Substring(validString.Length - 1), substituteString, String.Empty);
+
+                return validString;
+            }
+
+            catch (Exception e)
+            {
+                CrestronConsole.PrintLine("Error in ValidateString: {0}", e.Message);
+                return String.Empty;
+            }
+        }
+    }
+}
